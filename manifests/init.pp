8440fe30a09e4edbe2e3ff68655a609f23f847e2
Giovanni Sakti <giosakti@starqle.com>
2012-06-26 17:08:41 +0700

    automagically create gemrc and rvmrc file.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,5 +6,13 @@ class rvm($version='latest', $install_rvm=true) {
       'rvm::dependencies': stage => 'rvm-install';
       'rvm::system':       stage => 'rvm-install', version => $version;
     }
+    file { "/etc/gemrc":
+      ensure => file,
+      content => template('rvm/gemrc')
+    }
+    file { "/etc/rvmrc":
+      ensure => file,
+      content => template('rvm/rvmrc')
+    }
   }
 }
